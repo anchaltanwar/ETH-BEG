961f45a244f92fa9211fe22830c9400ebae974ec
Anchal Tanwar <86921001+anchaltanwar@users.noreply.github.com>
2023-08-19 10:50:23 +0530

    Update ETH_PROOF_BEG.sol

--- ETH_PROOF_BEG.sol
+++ ETH_PROOF_BEG.sol
@@ -1,33 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
-//creating a contract
-contract errorhandling{
-    uint public amount=0;
-    uint public quotient=0;
 
-    //creating a function with name RequireCheck and parameters balance and coins 
-    function check(uint balance, uint coins) public payable  {
-        amount+=coins;
-        //using require function and giving it a condition
-        require(balance>=10,"Sorry!! balance is less than 10. Transaction is not possible");
-    }
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
 
-    error Throw_error(string);
-    //creating another function 
-    function RevertCheck(uint balance, uint coins)public payable  {
-        amount+=coins;
-        //using if conditon and using revert function inside the if condition
-        if(balance<10)
-            revert Throw_error("Balance is less than 10. Transaction is not possible");
-    }
+contract MyToken {
 
-    //creating a function and using an assert function inside it
-    function AssertCheck(uint i, uint j) public {
-        assert(j!=0);
+    // public variables here
+   string public tokenName = "META";
+   string public tokenAbbrv = "MTA";
+   uint public totalSupply = 0;
 
-        quotient=i/j;
+    // mapping variable here
+    mapping(address => uint ) public balances; 
 
-    }
-    
+    // mint function
+   function mint (address _address, uint _value) public{
+      totalSupply += _value;
+      balances[_address] += _value;
+   }
+
+    // burn function
+   function burn(address _address, uint _value) public {
+      if(balances[_address] >= _value){
+         totalSupply -= _value;
+         balances[_address] -= _value;
+      }
+   }
 
 }
